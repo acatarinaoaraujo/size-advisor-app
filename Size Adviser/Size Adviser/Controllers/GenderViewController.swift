04f2aa56926f76d936370ab28701655d67baa0f7
acatarinaoaraujo <acoa_@hotmail.com>
2022-07-07 15:45:17 -1000

    update

---
+++ Size Adviser/Size Adviser/Controllers/GenderViewController.swift
@@ -0,0 +1,22 @@
+//
+//  GenderViewController.swift
+//  Size Adviser
+//
+//  Created by Ana Araujo on 7/7/22.
+//
+
+import UIKit
+
+class GenderViewController: UIViewController {
+    
+
+    @IBOutlet weak var adviceLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    
+
+}
+
