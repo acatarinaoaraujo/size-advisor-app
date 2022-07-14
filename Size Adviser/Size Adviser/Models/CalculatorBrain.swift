3222753642a7363cc8a9f860f3f552ba3410bba4
acatarinaoaraujo <acoa_@hotmail.com>
2022-07-13 20:03:49 -1000

    update

--- Size Adviser/Size Adviser/Models/CalculatorBrain.swift
+++ Size Adviser/Size Adviser/Models/CalculatorBrain.swift
@@ -41,25 +41,18 @@ struct CalculatorBrain {
     }*/
     
     mutating func calculateSize(gender: String, units: String, chest: inout Float, waist: inout Float, hips: inout Float) {
-        print(chest)
-        print(waist)
-        print(hips)
         
         if units == "cm" {
             chest = chest / 2.54
             waist = waist / 2.54
             hips = hips / 2.54
         }
-        
-        print(chest)
-        print(waist)
-        print(hips)
-        
+
         if gender == "WOMAN" {
             sizeForWoman(chest, waist, hips)
             
         } else {
-            //sizeForMan(chest, waist, hips, inseam)
+            sizeForMan(chest, waist, hips)
         }
     }
     
@@ -68,10 +61,14 @@ struct CalculatorBrain {
         let estimationWaist = sizeChart(waist, 25.5, 27.0, 28.5, 30.0, 31.5, 33.0, 34.5)
         let estimationHips = sizeChart(hips, 32.5, 34.0, 35.5, 37.0, 36.5, 38.0, 39.5)
         
-        print(estimationChest)
-        print(estimationWaist)
-        print(estimationHips)
+        size = Size(chest: estimationChest, waist: estimationWaist, hips: estimationHips)
+        
+    }
     
+    mutating func sizeForMan(_ chest: Float, _ waist: Float, _ hips: Float){
+        let estimationChest = sizeChart(chest, 31.0, 32.5, 34.0, 35.5, 37, 38.5, 40)
+        let estimationWaist = sizeChart(waist, 25.5, 27.0, 28.5, 30.0, 31.5, 33.0, 34.5)
+        let estimationHips = sizeChart(hips, 32.5, 34.0, 35.5, 37.0, 36.5, 38.0, 39.5)
         
         size = Size(chest: estimationChest, waist: estimationWaist, hips: estimationHips)
         
@@ -99,127 +96,7 @@ struct CalculatorBrain {
         }
     }
     
-    
-    /*mutating func womanChest(_ chest: Float) -> String {
-        
-        switch (chest) {
-        case ..<31.0:
-                return "XXS"
-        case ..<32.5:
-                return "XS"
-        case ..<34.0:
-                return "S"
-        case ..<35.5:
-                return "M"
-        case ..<36.5:
-                return "L"
-        case ..<38.0:
-                return "XL"
-        case ..<39.5:
-                return "XXL"
-        default: return "XXXL"
-
-        }
-    }
-    
-    mutating func womanWaist(_ waist: Float) -> String {
-        
-        switch (waist) {
-        case ..<25.5:
-                return "XXS"
-        case ..<27.0:
-                return "XS"
-        case ..<28.5:
-                return "S"
-        case ..<30.0:
-                return "M"
-        case ..<31.5:
-                return "L"
-        case ..<33.0:
-                return "XL"
-        case ..<34.5:
-                return "XXL"
-        default: return "XXXL"
-
-        }
-    }
-    
-    mutating func womanHips(_ hips: Float) -> String {
-        
-        switch (hips) {
-        case ..<32.0:
-                return "XXS"
-        case ..<33.5:
-                return "XS"
-        case ..<35.0:
-                return "S"
-        case ..<36.5:
-                return "M"
-        case ..<39.5:
-                return "L"
-        case ..<41.0:
-                return "XL"
-        case ..<42.5:
-                return "XXL"
-        default: return "XXXL"
-
-        }
-    }*/
-
-        
-       /* if chest < 31 {
-            // xxs
-        } else if chest < 33 {
-            // xs
-        } else if chest < 35 {
-            // s
-        } else if chest < 37 {
-            // m
-        } else if chest < 39 {
-            // l
-        } else if chest
+    /*mutating func calculateUS(_ chest: Float, _ waist: Float, _ hips: Float) {
         
     }*/
-    
-    /*mutating func sizeForMan(_ chest: Float, _ waist: Float, _ hips: Float, _ inseam: Float){
-        
-    }*/
-    
-    /* if let n = number {
-     switch symbol {
-     case "+/-":
-         return n * -1
-     case "AC":
-         return 0
-     case "%":
-         return n * 0.01
-     case "sin":
-         return sin(n)
-     case "cos":
-         return cos(n)
-     case "tan":
-         return tan(n)
-     case "√":
-         return n.squareRoot()
-     case "=":
-         return performTwoNumCalculation(n2: n)
-     default:
-         intermediateCalculation = (n1: n, calcMethod: symbol)
-     }
- } */
-    
-    
-    /*mutating func calculateBMI(height: Float, weight: Float) {
-        let bmiValue = weight / (height * height)
-
-        if bmiValue < 18.5 {
-            bmi = BMI(value: bmiValue, advice: "Eat more pies!", color: #colorLiteral(red: 0.4745098054, green: 0.8392156959, blue: 0.9764705896, alpha: 1))
-        } else if bmiValue < 24.9 {
-            bmi = BMI(value: bmiValue, advice: "Fit as a fiddle!", color: #colorLiteral(red: 0.721568644, green: 0.8862745166, blue: 0.5921568871, alpha: 1))
-        } else {
-            bmi = BMI(value: bmiValue, advice: "Eat less pies!", color: #colorLiteral(red: 0.9098039269, green: 0.4784313738, blue: 0.6431372762, alpha: 1))
-        }
-    }*/
-    
-    
 }
